b2490e0b521f37cfbae41f4b0eb0a7e37516ace9
Badhri Hari <114570849+badhri-hari@users.noreply.github.com>
2025-02-24 06:11:05 +0530

    Added repo link to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header>
       <div className="header-logo">wikiarticles</div>
-      <a href="https://github.com/badhri-hari/" target="_blank" rel="noreferrer noopener">
+      <a href="https://github.com/badhri-hari/wikiarticles" target="_blank" rel="noreferrer noopener">
         <VscGithubInverted color="#F8F8FF" size="2.7em" className="header-icon" />
       </a>
       <div />
