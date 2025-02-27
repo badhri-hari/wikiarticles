602376474e0fabb89b5e1f39018614c2a03e78d4
Badhri Hari <114570849+badhri-hari@users.noreply.github.com>
2025-02-27 14:44:16 +0530

    Increased articele loading count to 100

--- api/articles.js
+++ api/articles.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
 
   try {
-    const numArticles = 50;
+    const numArticles = 100;
     const summaryRequests = [];
     for (let i = 0; i < numArticles; i++) {
       summaryRequests.push(
